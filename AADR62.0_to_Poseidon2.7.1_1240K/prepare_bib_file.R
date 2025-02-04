8979dd73359916327e78effdf7dda7b86ca01f11
Clemens Schmid <clemens@nevrome.de>
2025-02-04 23:08:29 +0100

    completed the reworking of the prepare_bib_file.R script

--- AADR62.0_to_Poseidon2.7.1_1240K/prepare_bib_file.R
+++ AADR62.0_to_Poseidon2.7.1_1240K/prepare_bib_file.R
@@ -1,5 +1,13 @@
 library(magrittr)
 
+#### prepare DOI list ####
+
+complete_keys_and_dois <- readRDS("AADR62.0_to_Poseidon2.7.1_1240K/tmp/complete_keys_and_dois.rds")
+
+unqiue_dois <- complete_keys_and_dois$doi %>% unique()
+
+writeLines(unqiue_dois, "AADR62.0_to_Poseidon2.7.1_1240K/tmp/DOIs.txt")
+
 #### resolve DOIs to BibTeX entries ####
 
 # using doi2bib: https://github.com/bibcure/doi2bib
@@ -13,8 +21,7 @@ system(paste(
 #### clean resulting .bib file ####
 
 # validate result
-
-system("biber --tool --validate-datamodel AADR62.0_to_Poseidon2.7.1_1240K/tmp/References_raw.bib")
+# system("biber --tool --validate-datamodel AADR62.0_to_Poseidon2.7.1_1240K/tmp/References_raw.bib")
 
 # load result
 references <- bibtex::read.bib("AADR62.0_to_Poseidon2.7.1_1240K/tmp/References_raw.bib")
@@ -28,39 +35,56 @@ references <- bibtex::read.bib("AADR62.0_to_Poseidon2.7.1_1240K/tmp/References_r
 dois_actually_in_bibtex <- references %>% purrr::map_chr(\(x) x$doi)
 requested_dois <- readLines("AADR62.0_to_Poseidon2.7.1_1240K/tmp/DOIs.txt")
 
-setdiff(requested_dois, dois_actually_in_bibtex)
+setdiff(tolower(requested_dois), tolower(dois_actually_in_bibtex))
 
-# 2. manual step: add missing bibtex entries for some papers
-# 10.7554/eLife.79714 -> 
-# 10.1038/s41467-019-09209- -> 
-# 10.7554/eLife.85492 -> 
-# 10.1186/s13059-023-03013-103 -> 
-
-# 3. manual step: clean incorrectly formatted values
-# {PLoS ONE} {ONE} -> {PLoS ONE}
-# {Nat Ecol Evol}amp$\mathsemicolon$ Evolution} -> {Nat Ecol Evol}
-# {PLoS Biol} Biology} -> {PLoS Biol}
-# {PLoS Genet} Genetics} -> {PLoS Genet}
+# 2. manual step: add missing bibtex entries for one paper
+# https://reich.hms.harvard.edu/sites/reich.hms.harvard.edu/files/inline-files/10_24_2016_Screening_report_for_St_Marys_City_burials_FINAL_IL.pdf
 
 #### adjust citation keys in bib file ####
 
-in_references <- bibtex::read.bib("AADR54.1.p1_to_Poseidon2.7.0_1240K/tmp/References_raw.bib")
+in_references <- bibtex::read.bib("AADR62.0_to_Poseidon2.7.1_1240K/tmp/References_raw.bib")
 
-dois_in_bibtex <- in_references %>% purrr::map_chr(\(x) x$doi)
+dois_in_bibtex <- in_references %>% purrr::map_chr(\(x) {
+  if (is.null(x$doi)) { NA_character_ } else {x$doi}
+})
 keys_in_bibtex <- in_references %>% purrr::map_chr(\(x) attr(x, "key"))
-in_bibtex <- tibble::tibble(doi2bib_key = keys_in_bibtex, doi = dois_in_bibtex)
-merged_key_table <- dplyr::left_join(in_bibtex, final_doi_table, by = "doi")
+in_bibtex <- tibble::tibble(
+  bib_entry_index = 1:length(in_references),
+  doi2bib_key = keys_in_bibtex,
+  doi = dois_in_bibtex
+)
+in_bibtex$doi <- tolower(in_bibtex$doi)
+complete_keys_and_dois$doi <- tolower(complete_keys_and_dois$doi)
 
-out_references <- purrr::map2(
-  in_references, merged_key_table$key,
-  \(x, y) {
-    attr(x, "key") <- y
-    return(x)
+# note that this join introduces a number of doi duplicates!
+# the AADR includes the same publication under different publication keys
+merged_key_table <- dplyr::left_join(
+  in_bibtex,
+  complete_keys_and_dois,
+  by = "doi"
+)
+
+out_references <- purrr::pmap(
+  merged_key_table, function(bib_entry_index, doi2bib_key, doi, key) {
+    cur_reference <- in_references[[bib_entry_index]]
+    # see here for an explanation of the weird syntax below:
+    # https://stackoverflow.com/questions/9765493/how-do-i-reference-specific-tags-in-the-bibentry-class-using-the-or-conv
+    cur_reference$key <- key
+    return(cur_reference)
   }
 )
-class(out_references) <- "bibentry"
 
 #### write final .bib file #### 
 
-bibtex::write.bib(out_references, "AADR54.1.p1_to_Poseidon2.7.0_1240K/tmp/References.bib")
+file.remove("AADR62.0_to_Poseidon2.7.1_1240K/tmp/References.bib")
+purrr::walk(
+  out_references, function(bibentry) {
+    text <- format(bibentry, style = "bibtex")
+    write(
+      text,
+      file = "AADR62.0_to_Poseidon2.7.1_1240K/tmp/References.bib",
+      append = TRUE
+    )
+  }
+)
 
