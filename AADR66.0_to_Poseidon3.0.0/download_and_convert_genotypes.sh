c2203639a70e8bade0efb21e73f5efeddf15bc25
Clemens Schmid <clemens@nevrome.de>
2026-05-01 15:39:37 +0200

    genotype data transformation

--- AADR66.0_to_Poseidon3.0.0/download_and_convert_genotypes.sh
+++ AADR66.0_to_Poseidon3.0.0/download_and_convert_genotypes.sh
@@ -6,21 +6,22 @@ mkdir tmp
 # v66.1240K.aadr.PUB
 wget https://dataverse.harvard.edu/api/access/datafile/13663706 -O tmp/v66.1240K.aadr.PUB.anno
 wget https://dataverse.harvard.edu/api/access/datafile/13663698 -O tmp/v66.1240K.aadr.PUB.ind
-
-
+wget https://dataverse.harvard.edu/api/access/datafile/13663698 -O tmp/v66.1240K.aadr.PUB.ind
+wget https://dataverse.harvard.edu/api/access/datafile/13664080 -O tmp/v66.1240K.aadr.PUB.geno
 
 # convert to EIGENSTRAT
-#cat > tmp/convertf_parfile <<EOF
-#genotypename: tmp/v62.0_1240k_public.geno
-#snpname: tmp/v62.0_1240k_public.snp
-#indivname: tmp/v62.0_1240k_public.ind
-#outputformat: EIGENSTRAT
-#genotypeoutname: tmp/AADR_1240K.geno
-#snpoutname: tmp/AADR_1240K.snp
-#indivoutname: tmp/AADR_1240K.ind
-#EOF
+cat > tmp/convertf_parfile_AADR_v66_1240K <<EOF
+genotypename: tmp/v66.1240K.aadr.PUB.geno
+snpname: tmp/v66.1240K.aadr.PUB.snp
+indivname: tmp/v66.1240K.aadr.PUB.ind
+outputformat: EIGENSTRAT
+genotypeoutname: tmp/AADR_v66_1240K.geno
+snpoutname: tmp/AADR_v66_1240K.snp
+indivoutname: tmp/AADR_v66_1240K.ind
+EOF
 
-#convertf -p tmp/convertf_parfile
+convertf -p tmp/convertf_parfile_AADR_v66_1240K
 
 # convert to binary PLINK
 #trident genoconvert -p tmp/AADR_1240K.geno --outFormat PLINK
+
