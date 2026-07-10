bf94d0029be836e3cb5be57b07bb1c32d496280c
Clemens Schmid <nevromecs@gmail.com>
2026-07-10 18:59:03 +0200

    starting with the smalles package for testing

--- AADR66.p1_adjustments/subset_v66_packages.sh
+++ AADR66.p1_adjustments/subset_v66_packages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 trident forge -d ~/agora/aadr-archive/AADR_v66_2M --forgeFile ~/agora/aadr2poseidon/AADR66.p1_adjustments/remove_jacobs_forgefile.txt --preservePyml --zip -o ~/agora/aadr-archive/AADR_v66_p1_2M
+
 trident rectify -d ~/agora/aadr-archive/AADR_v66_p1_2M --checksumAll
 
+
+
+trident forge -d ~/agora/aadr-archive/AADR_v66_HO_compatibility --forgeFile ~/agora/aadr2poseidon/AADR66.p1_adjustments/remove_jacobs_forgefile.txt --preserve --zip -o ~/agora/aadr-archive/AADR_v66_p1_HO_compatibility
+trident rectify -d ~/agora/aadr-archive/AADR_v66_p1_HO_compatibility --checksumAll
